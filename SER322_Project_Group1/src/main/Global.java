1ac01fe2a97ae2486de0f32f1845c666e7c43316
tchase2 <tchase2@asu.edu>
2018-04-24 11:13:01 -0700

    more

--- SER322_Project_Group1/src/main/Global.java
+++ SER322_Project_Group1/src/main/Global.java
@@ -1,5 +1,23 @@
 package main;
 
-public class Global {
+import java.util.LinkedList;
 
+import entities.*;
+
+public class Global {
+	public static LinkedList<Ammo> ammoList = new LinkedList<>();
+	public static LinkedList<Armor> armorList = new LinkedList<>();
+	public static LinkedList<Backpack> backpackList = new LinkedList<>();
+	public static LinkedList<Booster> boosterList = new LinkedList<>();
+	public static LinkedList<Clothing> clothingList = new LinkedList<>();
+	public static LinkedList<HealingItem> healingItemList = new LinkedList<>();
+	public static LinkedList<Item> itemList = new LinkedList<>();
+	public static LinkedList<Map> mapList = new LinkedList<>();
+	public static LinkedList<Player> playerList = new LinkedList<>();
+	public static LinkedList<Session> sessionList = new LinkedList<>();
+	public static LinkedList<Standard> standardList = new LinkedList<>();
+	public static LinkedList<ThrowAble> throwableList = new LinkedList<>();
+	public static LinkedList<Vehicle> vehicleList = new LinkedList<>();
+	public static LinkedList<Weapon> weaponList = new LinkedList<>();
+	
 }
