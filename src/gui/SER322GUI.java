68e3a682c189bf136a40b284300f7dbecd04c57a
tchase2 <35580357+tchase2@users.noreply.github.com>
2018-04-26 15:33:44 -0700

    initialized entities

--- src/gui/SER322GUI.java
+++ src/gui/SER322GUI.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gui;
+import main.Global;
 
-/**
- *
- * @author natha
- */
 public class SER322GUI {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+    	//if (Global.ammoList.getList().size() < 1)
+    		Global.init();
     }
     
 }
--- src/main/Start.java
+++ src/main/Start.java
@@ -1,8 +1,9 @@
 package main;
 
 public class Start {
-
+	/*
 	public static void main(String[] args) {
 		
 	}
+	*/
 }
